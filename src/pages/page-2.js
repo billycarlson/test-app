a9763861e36b7cdb0f0d2ebe3b5ec124076453f6
Billy Carlson <billycarlson@skeelosmbpkehe.kehe.com>
2018-08-07 16:32:31 -0500

    all Finished

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,7 +3,8 @@ import Link from 'gatsby-link'
 
 const SecondPage = () => (
   <div>
-    <h1>Hi from the second page</h1>
+    <h1>Hi</h1>
+    <h2>Welcome to the best page on the internet</h2>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </div>
